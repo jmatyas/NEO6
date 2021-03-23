3e89964af9d74e9daf57d45294121639c14da0c2
jmatyas <jakubk.m@gmail.com>
2021-03-23 08:52:57 +0100

    Add parsing RMC, GSA and GGA statements; Receive from UART and send the same data to another one; Receive data using interrupts

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,38 @@
 use panic_semihosting as _;
 
 use core::cell::RefCell;
-use core::ops::{Deref, DerefMut};
+use core::ops::{DerefMut};
 
-// use embedded_hal::embedded_dma::{StaticReadBuffer, StaticWriteBuffer};
 
 use cortex_m_rt::entry;
 use cortex_m::singleton;
 use cortex_m::interrupt::{
     free,
-    CriticalSection,
     Mutex
 };
 
+mod neo;
+use neo::{MSG, NEO6};
+use nb::block;
+use embedded_hal::serial::Write;
 
 use stm32f1xx_hal as hal;
 use hal::{
     prelude::*,
     stm32,
-    dma::{self, Half, CircBuffer},
+    dma,
     delay::Delay,
-    serial::{self, Serial, Config, RxDma1, TxDma1},
+    serial::{self, Serial, Config, Rx1, Rx3, Tx1},
     stm32::{interrupt, NVIC},
-    gpio::{gpioa, Alternate, PushPull, Input, Floating},
+    // gpio::{gpioa, Alternate, PushPull, Input, Floating},
 };
-use cortex_m_semihosting::{hprintln, hprint};
-use nb::block;
+use cortex_m_semihosting::{hprintln};
 
-// static uart: Mutex<RefCell<Option<Serial<stm32::USART1, (gpio::gpiob::PB6<Alternate<PushPull>>, gpio::gpiob::PB7<Input<Floating>>)>>>> = Mutex::new(RefCell::new(None));
-// static TX: Mutex<RefCell<Option<serial::Tx<stm32::USART1>>>> = Mutex::new(RefCell::new(None));
-// static RX: Mutex<RefCell<Option<serial::Rx<stm32::USART1>>>> = Mutex::new(RefCell::new(None));
-
-static G_UART: Mutex<RefCell<Option<Serial<stm32::USART1, (gpioa::PA9<Alternate<PushPull>>, gpioa::PA10<Input<Floating>>)>>>> = Mutex::new(RefCell::new(None));
-static G_RXDMA: Mutex<RefCell<Option<RxDma1>>> = Mutex::new(RefCell::new(None));
-static G_TXDMA: Mutex<RefCell<Option<TxDma1>>> = Mutex::new(RefCell::new(None));
-static G_CIRC_BUFFER: Mutex<RefCell<Option<CircBuffer<[u8; 10], RxDma1>>>> = Mutex::new(RefCell::new(None));
+// static G_UART: Mutex<RefCell<Option<Serial<stm32::USART1, (gpioa::PA9<Alternate<PushPull>>, gpioa::PA10<Input<Floating>>)>>>> = Mutex::new(RefCell::new(None));
+// static G_RXDMA: Mutex<RefCell<Option<RxDma1>>> = Mutex::new(RefCell::new(None));
+// static G_TXDMA: Mutex<RefCell<Option<TxDma1>>> = Mutex::new(RefCell::new(None));
+// static G_SERIAL_CIRC: Mutex<RefCell<Option<SerialCircDMA<RxDma1>>>> = Mutex::new(RefCell::new(None));
+static G_NEO: Mutex<RefCell<Option<NEO6<Rx3, Tx1>>>> = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
@@ -60,15 +58,15 @@ fn main() -> ! {
     let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
     let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
     let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let mut _gpioc = dp.GPIOC.split(&mut rcc.apb2);
 
     let mut dma_channels = dp.DMA1.split(&mut rcc.ahb);
 
 
     let mut delay = Delay::new(cp.SYST, clocks);
 
-    let mut tx_pin = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
-    let mut rx_pin = gpioa.pa10.into_floating_input(&mut gpioa.crh);
+    let tx_pin = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+    let rx_pin = gpioa.pa10.into_floating_input(&mut gpioa.crh);
 
     let mut serial = Serial::usart1(
         dp.USART1,
@@ -82,129 +80,78 @@ fn main() -> ! {
     );
 
     // serial.listen(serial::Event::Rxne);
-    serial.listen(serial::Event::Idle);
-    let (mut log_tx, mut log_rx) = serial.split();
-    dma_channels.5.listen(dma::Event::HalfTransfer);
-    dma_channels.5.listen(dma::Event::TransferComplete);
-    let mut log_rx_dma = log_rx.with_dma(dma_channels.5);
-    let mut log_tx_dma = log_tx.with_dma(dma_channels.4);
-    
-    let gps_buff = singleton!(: [[u8; 10]; 2] = [[0; 10]; 2]).unwrap();
-    let mut circ_buffer = log_rx_dma.circ_read(gps_buff);
+    // serial.listen(serial::Event::Txe);
+    // serial.listen(serial::Event::Tc);
+    // serial.listen(serial::Event::Idle);
+    let (mut log_tx, log_rx) = serial.split();
+    for byte in b"ADAS" {
+        block!(log_tx.write(*byte)).ok();
+    }
+    const LEN: usize = 100;
 
-    // log_rx_dma.listen(serial::Event::Idle);    
+    let tx_buff = singleton!(: [u8; LEN] = [0; LEN] ).unwrap();
+    let mut msg_buff = [0u8; 1024];
+    let msg_len = msg_buff.len();
+
+
+    let mut tx_pin = gpiob.pb10.into_alternate_push_pull(&mut gpiob.crh);
+    let mut rx_pin = gpiob.pb11.into_floating_input(&mut gpiob.crh);
+
+    let mut gps_serial = Serial::usart3(
+        dp.USART3,
+        (tx_pin, rx_pin),
+        &mut afio.mapr,
+        Config::default()
+                .baudrate(9600.bps())
+                .parity_none(),
+        clocks,
+        &mut rcc.apb1,
+    );
+
+    gps_serial.listen(serial::Event::Rxne);
+
+    let (gps_tx, gps_rx) = gps_serial.split();
+    let mut neo = NEO6::new(tx_buff, gps_rx, log_tx);
+    // let mut msg = MSG::new(&mut msg_buff, msg_len);
     
+
+
     free(|cs| {
-        G_CIRC_BUFFER.borrow(cs).replace(Some(circ_buffer));
-        G_TXDMA.borrow(cs).replace(Some(log_tx_dma));
+        G_NEO.borrow(cs).replace(Some(neo));
     });
 
-    NVIC::unpend(stm32::Interrupt::DMA1_CHANNEL5);
-    NVIC::unpend(stm32::Interrupt::USART1);
+    NVIC::unpend(stm32::Interrupt::USART3);
     unsafe {
-        NVIC::unmask(stm32::Interrupt::DMA1_CHANNEL5);
-        NVIC::unmask(stm32::Interrupt::USART1);
+        NVIC::unmask(stm32::Interrupt::USART3);
     };
+    delay.delay_ms(500u16);
 
-    // let log_rx_dma = log_rx.with_dma(dma_channels.5);
 
-
-    // let mut tx_pin = gpiob.pb10.into_alternate_push_pull(&mut gpiob.crh);
-    // let mut rx_pin = gpiob.pb11.into_floating_input(&mut gpiob.crh);
-
-    // let mut gps_serial = Serial::usart3(
-    //     dp.USART3,
-    //     (tx_pin, rx_pin),
-    //     &mut afio.mapr,
-    //     Config::default()
-    //             .baudrate(9600.bps())
-    //             .parity_none(),
-    //     clocks,
-    //     &mut rcc.apb1,
-    // );
-
-    // let (mut gps_tx, mut gps_rx) = gps_serial.split();
-    // let gps_rx_dma = gps_rx.with_dma(dma_channels.3);
-    
-    // for byte in b"HelloMordo\r\n" {
-    //     block!(log_tx.write(*byte)).unwrap();
-    // }
-
+   
 
     loop {
-        // while circ_buffer.readable_half().unwrap() != Half::First {}
-        // let _first_half = circ_buffer.peek(|half, _| *half).unwrap();
-
-        // for byte in _first_half.iter() {
-        //     block!(log_tx.write(*byte)).unwrap();
-        // }
-    
-        // while circ_buffer.readable_half().unwrap() != Half::Second {}
-        // let _second_half = circ_buffer.peek(|half, _| *half).unwrap();
-    
-    
-       
-    
-        // for byte in _second_half.iter() {
-        //     block!(log_tx.write(*byte)).unwrap();
-        // }
+        // neo.receive();
+        free(|cs| {
+            let mut neo_ref = G_NEO.borrow(cs).borrow_mut();
+            if let Some(ref mut neo) = neo_ref.deref_mut() {
+                neo.parse();
+                if neo.is_valid() {
+                    neo.unlisten();
+                };
+            }
+        });
+        
     }
 }
 
-
 #[interrupt]
-fn USART1() {
-    // static mut UART: Option<Serial<stm32::USART1, (gpioa::PA9<Alternate<PushPull>>, gpioa::PA10<Input<Floating>>)>> = None;
-    free(|cs|{
-        let mut circ_buffer_ref = G_CIRC_BUFFER.borrow(cs).borrow_mut();
-        if let Some(ref mut circ_buffer) = circ_buffer_ref.deref_mut() {
-            // hprintln!("{:?}", circ_buffer.payload.which_event());
-            circ_buffer.payload.clear_event();
-            // circ_buffer.stop();
-            // hprintln!("USART")
-            ()
-        // if let Some(ref mut rxdma) = rx_dma_ref.deref_mut() {
-            // if let Some(ev) = rxdma.which_event() {
-            //     match ev {
-            //         serial::Event::Idle => { hprintln!("Idle")},
-            //         serial::Event::Rxne => { 
-            //             hprintln!("Rxne")
-            //         },
-            //         _ => hprintln!("Tx"),
-            //     }
-            } else {
-                // hprintln!("D")
-            };
-            // uart.clear_event()
+fn USART3() {
+    free(|cs| {
+        // hprintln!("d");
+        let mut neo_ref = G_NEO.borrow(cs).borrow_mut();
+        if let Some(ref mut neo) = neo_ref.deref_mut() {
+            neo.receive();
+        }
 
-        // }
-    });
-}
-
-#[interrupt]
-fn DMA1_CHANNEL5() {
-    // static mut UART: Option<Serial<stm32::USART1, (gpioa::PA9<Alternate<PushPull>>, gpioa::PA10<Input<Floating>>)>> = None;
-    free(|cs|{
-        
-        let mut circ_buffer_ref = G_CIRC_BUFFER.borrow(cs).borrow_mut();
-        let mut txdma_ref = G_TXDMA.borrow(cs).borrow_mut();
-        if let (Some(ref mut circ_buffer), Some(tx_dma)) = (circ_buffer_ref.deref_mut(), txdma_ref.deref_mut()) {
-
-            let half = match circ_buffer.readable_half().unwrap() {
-                Half::First => {hprintln!("Pierwszy"); Half::First},
-                Half::Second => {hprintln!("Drugi"); Half::Second},
-            };
-            let _msg = circ_buffer.peek(|half, _| *half).unwrap();
-            // tx.write(_msg).wait();
-            for byte in _msg.iter(){
-                hprint!("{:?}", *byte as char);
-            }
-            // };
-            // uart.clear_event()
-            } else {
-                // hprintln!("Duza dua")
-                ()
-            }
-        // }
     });
 }
---
+++ src/neo.rs
@@ -0,0 +1,517 @@
+extern crate embedded_hal;
+use embedded_hal::serial::{Read, Write};
+use nb::block;
+use cortex_m_semihosting::{hprintln, hprint};
+use core::sync::atomic::{self, Ordering};
+
+use crate::stm32::{USART1, USART3};
+use crate::serial::Event as SEvent;
+use crate::{Rx1, Tx1, Rx3};
+
+pub fn atoi(barray: & [u8]) -> u32 {
+    let mut value = 0u32;
+    let arr_iter = barray.iter();
+    for element in barray.iter() {
+        value = value*10 + *element as u32 - b'0' as u32;
+    }
+    value
+}
+
+#[derive(Debug)]
+pub enum GPS_Statement {
+    GPRMC,
+    GPGSA,
+    GPGGA,
+    GPGSV,
+    Other,
+}
+
+#[derive(Debug)]
+pub enum Validity {
+    Valid,
+    NotValid,
+}
+#[derive(Debug)]
+pub enum Half {
+    First,
+    Second,
+}
+
+#[derive(Debug)]
+pub struct GPSTime {
+    hour: u8,
+    minute: u8,
+    second: u8,
+}
+
+impl GPSTime {
+    pub fn new() -> Self {
+        GPSTime {
+            hour: 0,
+            minute: 0,
+            second: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct GPSDate {
+    day: u8,
+    month: u8,
+    year: u8,
+}
+
+
+impl GPSDate {
+    pub fn new() -> Self {
+        GPSDate {
+            day: 0,
+            month: 0,
+            year: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Position {
+    lattitude: f32,
+    ns_indicator: char,
+    longitude: f32,
+    ew_indicator: char,
+    // altitude: f32,
+}
+
+impl Position {
+    pub fn new() -> Self {
+        Position {
+            lattitude: 0.0,
+            ns_indicator: 'N',
+            longitude: 0.0,
+            ew_indicator: 'E',
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct GPSSatellite {
+    ID: u8,
+    elevation: u8,
+    azimuth: u16,
+    SNR: u8,
+}
+
+#[derive(Debug)]
+pub enum FixType {
+    NoFix,
+    GPSFix,
+    DifferentialFix,
+}
+
+#[derive(Debug)]
+pub enum FixMode {
+    NoFix,
+    D2,
+    D3,
+}
+
+pub struct RMC {
+    valid: bool,
+    date: GPSDate,
+}
+
+pub struct GGA {
+    position: Position,
+    satellites_used: u8,
+    fix: FixType,
+    time: GPSTime,
+}
+
+pub struct GSA {
+    hdop: f32,
+    vdop: f32,
+    pdop: f32,
+    fix: FixMode,
+    satellite_id: [u8; 12],
+}
+
+
+pub struct MSG <'a> {
+    buffer: &'a mut [u8],
+    start: bool,
+    ptr: usize,
+    cap: usize,
+    len: usize,
+    last_read: Half,
+}
+
+impl <'a> MSG <'a> {
+    pub fn new(buff:&'a mut[u8], capactiy: usize) -> Self {
+        MSG {
+            buffer: buff,
+            start: false,
+            ptr: 0,
+            cap: capactiy,
+            len: 0,
+            last_read: Half::Second,
+        }
+    }
+    pub fn add(&mut self, c: u8) {
+        match self.last_read {
+            Half::Second => {
+                if self.ptr < self.cap {
+                    if c == b'$' {
+                        self.start = true;
+                        self.buffer[self.ptr] = c;
+                        self.ptr += 1;
+                    } else if c == b'\n' {
+                        self.buffer[self.ptr] = c;
+                        self.ptr = self.cap;
+                        self.last_read = Half::First;
+                        self.len += 1;
+                        self.start=false;
+                    } else if self.start {
+                        self.buffer[self.ptr] = c;
+                        self.ptr += 1;
+                    }
+                }
+            },
+            Half::First => {
+                if self.ptr < 2*self.cap {
+                    if c == b'$' {
+                        self.start = true;
+                        self.buffer[self.ptr] = c;
+                        self.ptr += 1;
+                    } else if c == b'\n' {
+                        self.buffer[self.ptr] = c;
+                        self.ptr = 0;
+                        self.last_read = Half::Second;
+                        self.len += 1;
+                        self.start = false;
+                    } else if self.start {
+                        self.buffer[self.ptr] = c;
+                        self.ptr += 1;
+                    }
+                }
+            },
+        }
+    }
+
+    pub fn clear(&mut self) {
+        // match self.last_read {
+        //     Half::First => {
+        //         for byte in self.buffer[..self.cap].iter_mut(){
+        //             // hprint!("{:?}", *byte as char);
+        //             *byte = 0;
+        //         }
+        //     },
+        //     Half::Second => {
+        //         for byte in self.buffer[self.cap..].iter_mut(){
+        //             // hprint!("{:?}", *byte as char);
+        //             *byte = 0;
+        //         }
+        //     }
+        // };
+        self.len -= 1;
+    }
+    pub fn is_full(&self) -> bool {
+        let tmp = if self.len == 2 {true} else {false};
+        tmp
+    }
+    pub fn is_empty(&self) -> bool {
+        let tmp = if self.len == 0 {true} else {false};
+        tmp
+    }
+
+
+    pub fn get_line(&self) -> (GPS_Statement, &[u8]) {
+        if self.len > 0 {
+            let mut data = match self.last_read {
+                Half::First => &self.buffer[..self.cap],
+                Half::Second => &self.buffer[self.cap..],
+            };
+            let mut msg = data.splitn(2, |c| *c == 0x2C);       // 0d44 split at ,
+            let (cmd, info) = (msg.next().unwrap(), msg.next().unwrap());
+            // hprintln!("cmd {:?}, info {:?}", cmd, info);
+            let (cmd, info) = match cmd {
+                b"$GPRMC" => { (GPS_Statement::GPRMC, info) },
+                b"$GPGSA" => { (GPS_Statement::GPGSA, info) },
+                b"$GPGGA" => { (GPS_Statement::GPGGA, info) },
+            //     // b"$GPGSV" => {hprintln!("GPGSV");},
+                _ => { (GPS_Statement::Other, info) },
+
+            };
+            return (cmd, info);
+        }
+        (GPS_Statement::Other, &[0u8; 1])
+    }
+}
+
+
+
+pub struct NEO6 <'a, Rx3, Tx1> {
+    rx: Rx3,
+    tx: Tx1,
+    buffer: MSG<'a>,
+    GPStime: GPSTime,
+    GPSdate: GPSDate,
+    validity: Validity,
+    position: Position,
+    // lattitude: f64,
+    // lattitude_drection: u8,
+    // longitude: f64,
+    // longitude_drection: u8,
+    // altitude: f64,
+
+    // speed_knots: f64,
+    // speed_km: f64,
+
+    // sat_num: u8,
+    // quality: u8,
+    // fix_mode: u8,
+    
+}
+
+impl <'a> NEO6 <'a, Rx3, Tx1> {
+    pub fn new(buf: &'a mut [u8], rx: Rx3, tx: Tx1) -> Self {
+        let buf_len =buf.len();
+        NEO6 {
+            rx: rx,
+            tx: tx,
+            buffer: MSG::new(buf, buf_len/2),
+            
+            //
+            GPStime: GPSTime::new(),
+            GPSdate: GPSDate::new(),
+            validity: Validity::NotValid,
+            position: Position::new(),
+        }
+    }
+    pub fn listen(&mut self) {
+        self.rx.listen(crate::serial::Event::Rxne);
+    }
+    pub fn unlisten(&mut self) {
+        self.rx.unlisten(crate::serial::Event::Rxne);
+    }
+    pub fn is_valid(&self) -> bool {
+        let valid  = match self.validity {
+            Validity::Valid => {
+                true
+            },
+            Validity::NotValid => {
+                false
+            },
+        };
+        valid
+    }
+    pub fn receive(&mut self) {
+        let mut i = 0;
+        // block!(self.tx.write(b'a')).ok();
+        if let Some(ev) = self.rx.which_event() {
+            match ev {
+                SEvent::Rxne => {
+                    self.rx.clear_event();
+                    if !self.buffer.is_full() {
+                        let a = unsafe { (*USART3::ptr()).dr.read().bits() as u8};
+
+                        // let a = block!(self.rx.read()).unwrap();
+                        self.buffer.add(a);
+                        block!(self.tx.write(a)).ok();
+                        // hprintln!("{:?}", a as char);
+                        i += 1;
+                    }
+                },
+                _ => {
+                    self.rx.clear_event();
+                }
+            }
+        }
+    }
+    pub fn get_line(& self) -> (GPS_Statement, &[u8]) {
+        self.buffer.get_line()
+    }
+    pub fn parse(&mut self) {
+        if !self.buffer.is_empty() {
+            // hprintln!("s");
+            let (mut cmd, mut info) = self.get_line();
+            match cmd {
+                    GPS_Statement::GPRMC => { 
+                        let rmc_data = self.parse_rmc(info);
+                        self.GPSdate = rmc_data.date;
+                    },
+                    GPS_Statement::GPGSA => {
+                        let gsa_data = self.parse_gsa(info);
+                    },
+                    GPS_Statement::GPGGA => {
+                        let gga_data = self.parse_gga(info);
+                    },
+                    _ => (),
+
+            }
+        
+            self.buffer.clear();
+        }
+    }
+    pub fn parse_rmc(&self, data: &[u8]) -> (RMC) {
+        let mut GPSdate = GPSDate::new();
+        let mut validity = false;
+
+        for (i, field) in data.split(|c| *c == b',').enumerate() {
+            if field.len() > 0 {
+                match i {
+                    // GPSTIME
+                    0 => {
+                        let (hour, minute, seconds) = (atoi(&field[2..]) as u8, atoi(&field[4..2]) as u8, atoi(& field[6..4]) as u8);
+                    },
+                    // Receiver Validity
+                    1 => {
+                        validity = if field[0] == b'A' {
+                            true
+                        } else {
+                            false
+                        };
+                    },
+                    // Lattitude
+                    2 => {
+                    },
+                    // HEMISPHERE indicator
+                    3 => {
+                    },
+                    // LONGITUDE
+                    4 => {
+                    },
+                    // H ind
+                    5 => {
+                    },
+                    // Speed over ground
+                    6 => {
+
+                    },
+                    // Course over ground
+                    7 => {
+
+                    },
+                    // GPSDATE
+                    8 => {
+                        let (day, month, year) = (atoi(&field[2..]) as u8, atoi(&field[4..2]) as u8, atoi(&field[6..4]) as u8);
+                        GPSdate = GPSDate{day, month, year};
+                    },
+                    _ => (),
+                }
+            }
+        } 
+        RMC {
+            valid: validity, 
+            date: GPSdate,
+        }
+    }
+    pub fn parse_gsa(&self, data: &[u8]) -> (GSA) {
+        let mut fixmode = FixMode::NoFix;
+        let mut sat_ids = [0u8; 12];
+        let mut hdop= 0.0f32;
+        let mut vdop= 0.0f32;
+        let mut pdop= 0.0f32;
+
+        for (i, field) in data.split(|c| *c == b',').enumerate() {
+            if field.len() > 0 {
+                match i {
+                    // Mode 1
+                    0 => {
+
+                    },
+                    // Mode 2 - FixMode
+                    1 => {
+                        fixmode = match atoi(&field) {
+                            2 => FixMode::D2,
+                            3 => FixMode::D3,
+                            _ => FixMode::NoFix,
+                        };
+                    },
+                    // IDs of sattelites in use
+                    2..=12 => {
+                        let id = atoi(&field) as u8;
+                        sat_ids[i-2] = id;
+                    },
+                    // Position Dilution of Precision
+                    13 => {
+                        let (int, fract) = (field[0] as u32, field[3] as u32);
+                        pdop = int as f32 + (fract as f32)/100.0;
+                    },
+                    // Horizontal Dilution of Precision
+                    14 => {
+                        let (int, fract) = (field[0] as u32, field[3] as u32);
+                        hdop = int as f32 + (fract as f32)/100.0;
+                    },
+                    // Vertical Dilution of Precision
+                    15 => {
+                        let (int, fract) = (field[0] as u32, field[3] as u32);
+                        vdop = int as f32 + (fract as f32)/100.0;
+                    },
+                    _ => (),
+                }
+            }
+        }
+        GSA {
+            hdop: hdop,
+            vdop: vdop,
+            pdop: pdop,
+            fix: fixmode,
+            satellite_id: sat_ids,
+        }
+    }
+    pub fn parse_gga(&self, data: &[u8]) -> (GGA) {
+        let mut GPStime = GPSTime::new();
+        let mut pos = Position::new();
+        let mut satellites = 0u8;
+        let mut fix_mode = FixType::NoFix;
+
+        for (i, field) in data.split(|c| *c == b',').enumerate() {
+            if field.len() > 0 {
+                match i {
+                    // UTC time
+                    0 => {
+
+                    },
+                    // Lattitude
+                    1 => {
+                        let lat = atoi(&field[4..]);
+                        pos.lattitude = (lat as f32)/100.0;
+                    },
+                    // N/S indicator
+                    2 => {
+                        pos.ns_indicator = field[0] as char;
+                    },
+                    // Longitude
+                    3 => {
+                        let long = atoi(&field[4..]);
+                        pos.longitude = (long as f32)/100.0;
+                    },
+                    // E/W indicator
+                    4 => {
+                        pos.ew_indicator = field[0] as char;
+                    },
+                    // FIX
+                    5 => {
+                        fix_mode = match atoi(&field) {
+                            0 => FixType::NoFix,
+                            1 => FixType::GPSFix,
+                            2 => FixType::DifferentialFix,
+                            _ => FixType::NoFix,
+                        };
+                    },
+                    // Satellites used
+                    6 => {
+                        satellites = atoi(&field) as u8;
+                    },
+                    _ => (),
+                }
+            }
+        }
+        GGA {
+            time: GPStime, 
+            position: pos,
+            satellites_used: satellites,
+            fix: fix_mode,
+        }
+
+    }
+}
